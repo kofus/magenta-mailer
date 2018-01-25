50de79e6f3eb12d244fd42c3a99fcd6254b8ab31
Ingo Bernhardt <bernhardt@kofus.de>
2018-01-25 16:30:10 +0100

    batch import

--- Kofus/Mailer/src/Kofus/Mailer/Controller/NewsChannelController.php
+++ Kofus/Mailer/src/Kofus/Mailer/Controller/NewsChannelController.php
@@ -31,4 +31,39 @@ class NewsChannelController extends AbstractActionController
             'subscriptions' => $subscriptions
         ));
     }
+    
+    public function batchImportAction()
+    {
+        $dt = new \DateTime();
+        $channel = $this->nodes()->getNode($this->params('id'), 'NCH');
+        $stream = file_get_contents('data/emails.txt');
+        foreach (explode("\n", $stream) as $line) {
+            $email = trim(strtolower($line));
+            if ($email) {
+                print $email . ' ';
+                $subscriber = $this->nodes()->getRepository('SCB')->findOneBy(array('emailAddress' => $email));
+                if (! $subscriber) {
+                    $subscriber = $this->nodes()->createNode('SCB');
+                    $subscriber->setEmailAddress($email);
+                    $this->em()->persist($subscriber);
+                    $this->em()->flush();
+                }
+                print $subscriber->getNodeId() . ' ';
+                
+                $subscription = $this->nodes()->getRepository('SCP')->findOneBy(array('subscriberId' => $subscriber->getNodeId(), 'channel' => $channel));
+                if (! $subscription) {
+                    $subscription = new \Kofus\Mailer\Entity\NewsSubscriptionEntity();
+                    $subscription->setChannel($channel)
+                        ->setSubscriberId($subscriber->getNodeId())
+                        ->setTimestampActivationSubscriber($dt);
+                    $this->em()->persist($subscription);
+                    $this->em()->flush();
+                }
+                print $subscription->getNodeId() . ' ';
+                
+                print '<br>';
+            }
+        }
+        die('DONE');
+    }
 }
