d719a33855cde1bd604dcfc12cd5a7625609622f
Ingo Bernhardt <bernhardt@kofus.de>
2019-07-31 10:38:39 +0200

    compatible with older versions

--- Kofus/Mailer/src/Kofus/Mailer/Service/MailerService.php
+++ Kofus/Mailer/src/Kofus/Mailer/Service/MailerService.php
@@ -161,6 +161,51 @@ class MailerService extends AbstractService implements EventManagerAwareInterfac
         }
         return $html;
     }
+    public function createMail($key='default', $template=null, array $values=array(), array $params=array())
+    {
+        $config = $this->config()->get('mailer.message.' . $key);
+        if (! $config)
+            throw new \Exception('Mail message "'.$key.'" not found in config');
+            
+            if (! isset($config['class']))
+                throw new \Exception('No class specification found for mail message "'.$key.'"');
+                
+                // Create class
+                $classname = $config['class'];
+                $mail = new $classname();
+                if (! $mail instanceof \Kofus\Mailer\Mail\MailInterface)
+                    throw new \Exception($classname . ' must implement MailInterface');
+                    
+                    // Merge values
+                    if ($this->config()->get('mailer.values'))
+                        $values = array_merge($this->config()->get('mailer.values'), $values);
+                        if (isset($config['values']))
+                            $values = array_merge($config['values'], $values);
+                            $mail->setValues($values);
+                            
+                            // Merge params
+                            if ($this->config()->get('mailer.params'))
+                                $params = array_merge($this->config()->get('mailer.params'), $params);
+                                if (isset($config['params']))
+                                    $params = array_merge($config['params'], $params);
+                                    $mail->setParams($params);
+                                    
+                                    // Template
+                                    if (! $template && isset($config['template']))
+                                        $template = $config['template'];
+                                        if ($template) {
+                                            $templateNode = $this->nodes()->getNode($template, 'MTMPL');
+                                            if (! $templateNode)
+                                                throw new \Exception('Mail template "'.$template.'" not found');
+                                                $mail->setTemplate($templateNode);
+                                        }
+                                        
+                                        // ServiceLocator?
+                                        if ($mail instanceof ServiceLocatorAwareInterface)
+                                            $mail->setServiceLocator($this->getServiceLocator());
+                                            
+                                            return $mail;
+    }
     
     public function createHtmlMessage(array $viewParams=array(), array $tokens=array(), $layout='default')
     {
